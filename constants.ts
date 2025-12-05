d632ea5f6beac8815bff3da1955c7a7ef693462b
Jacob <70430960+Jacob7179@users.noreply.github.com>
2025-12-05 10:51:21 +0800

    feat: Update INITIAL_SONGS with new entries and enhanced metadata

--- constants.ts
+++ constants.ts
@@ -1,49 +1,215 @@
 import { Song } from './types';
 
-// Updated with Video IDs that are typically "Lyric Videos" or "Audio" 
-// to avoid "Official Music Video" restriction (Error 150).
 export const INITIAL_SONGS: Song[] = [
   {
-    id: '1',
-    title: 'Shape of You',
-    artist: 'Ed Sheeran',
-    thumbnail: 'https://i.ytimg.com/vi/_dK2tDK9grQ/hqdefault.jpg',
-    videoId: '_dK2tDK9grQ' // Lyric Video
+    id: "LmZD-TU96q4",
+    title: "米津玄師  Kenshi Yonezu - IRIS OUT",
+    artist: "Kenshi Yonezu  米津玄師",
+    thumbnail: "https://i.ytimg.com/vi/LmZD-TU96q4/hqdefault.jpg",
+    videoId: "LmZD-TU96q4"
   },
   {
-    id: '2',
-    title: 'Blinding Lights',
-    artist: 'The Weeknd',
-    thumbnail: 'https://i.ytimg.com/vi/fHI8X4OXluQ/hqdefault.jpg',
-    videoId: 'fHI8X4OXluQ' // Official Audio
+    id: "Uh6dkL1M9DM",
+    title: "米津玄師  - Flamingo / Kenshi Yonezu",
+    artist: "Kenshi Yonezu  米津玄師",
+    thumbnail: "https://i.ytimg.com/vi/Uh6dkL1M9DM/hqdefault.jpg",
+    videoId: "Uh6dkL1M9DM"
   },
   {
-    id: '3',
-    title: 'Levitating',
-    artist: 'Dua Lipa',
-    thumbnail: 'https://i.ytimg.com/vi/35y_h270-dY/hqdefault.jpg',
-    videoId: '35y_h270-dY' // Lyric Video
+    id: "DpDCJrZL4FU",
+    title: "SEKAI NO OWARI「琥珀」",
+    artist: "SEKAI NO OWARI",
+    thumbnail: "https://i.ytimg.com/vi/DpDCJrZL4FU/hqdefault.jpg",
+    videoId: "DpDCJrZL4FU"
   },
   {
-    id: '4',
-    title: 'Peaches',
-    artist: 'Justin Bieber',
-    thumbnail: 'https://i.ytimg.com/vi/i7e8a9f-F6k/hqdefault.jpg',
-    videoId: 'i7e8a9f-F6k' // Visualizer
+    id: "lxvTWbSuj3c",
+    title: "SEKAI NO OWARI「図鑑」",
+    artist: "SEKAI NO OWARI",
+    thumbnail: "https://i.ytimg.com/vi/lxvTWbSuj3c/hqdefault.jpg",
+    videoId: "lxvTWbSuj3c"
   },
   {
-    id: '5',
-    title: 'Stay',
-    artist: 'The Kid LAROI & Justin Bieber',
-    thumbnail: 'https://i.ytimg.com/vi/yWRdK19bX9g/hqdefault.jpg',
-    videoId: 'yWRdK19bX9g' // Lyric Video
+    id: "QlyW9kYHTDo",
+    title: "Time Machine",
+    artist: "SEKAI NO OWARI - Topic",
+    thumbnail: "https://i.ytimg.com/vi/QlyW9kYHTDo/hqdefault.jpg",
+    videoId: "QlyW9kYHTDo"
   },
   {
-    id: '6',
-    title: 'Montero',
-    artist: 'Lil Nas X',
-    thumbnail: 'https://i.ytimg.com/vi/gJ9s3_91vKM/hqdefault.jpg',
-    videoId: 'gJ9s3_91vKM' // Lyric Video
+    id: "keOnleW2eak",
+    title: "Official髭男dism - らしさ [Official Video]",
+    artist: "Official髭男dism",
+    thumbnail: "https://i.ytimg.com/vi/keOnleW2eak/hqdefault.jpg",
+    videoId: "keOnleW2eak"
+  },
+  {
+    id: "CsQNfkaG5z4",
+    title: "SEKAI NO OWARI「イルミネーション」",
+    artist: "sekainoowarijp",
+    thumbnail: "https://i.ytimg.com/vi/CsQNfkaG5z4/hqdefault.jpg",
+    videoId: "CsQNfkaG5z4"
+  },
+  {
+    id: "0ZLN0xqZFkE",
+    title: "恋の去り際",
+    artist: "OFFICIAL HIGE DANDISM - Topic",
+    thumbnail: "https://i.ytimg.com/vi/0ZLN0xqZFkE/hqdefault.jpg",
+    videoId: "0ZLN0xqZFkE"
+  },
+  {
+    id: "SX_ViT4Ra7k",
+    title: "米津玄師  Kenshi Yonezu  - Lemon",
+    artist: "Kenshi Yonezu  米津玄師",
+    thumbnail: "https://i.ytimg.com/vi/SX_ViT4Ra7k/hqdefault.jpg",
+    videoId: "SX_ViT4Ra7k"
+  },
+  {
+    id: "BmmK7VkVu7g",
+    title: "不器用な二人で",
+    artist: "Yamane Marina - Topic",
+    thumbnail: "https://i.ytimg.com/vi/BmmK7VkVu7g/hqdefault.jpg",
+    videoId: "BmmK7VkVu7g"
+  },
+  {
+    id: "A7cp6OVa0Qc",
+    title: "Official髭男dism - Same Blue [Official Video]",
+    artist: "Official髭男dism",
+    thumbnail: "https://i.ytimg.com/vi/A7cp6OVa0Qc/hqdefault.jpg",
+    videoId: "A7cp6OVa0Qc"
+  },
+  {
+    id: "DuMqFknYHBs",
+    title: "Official髭男dism - イエスタデイ［Official Video］",
+    artist: "Official髭男dism",
+    thumbnail: "https://i.ytimg.com/vi/DuMqFknYHBs/hqdefault.jpg",
+    videoId: "DuMqFknYHBs"
+  },
+  {
+    id: "Vv2n31gL85I",
+    title: "Official髭男dism - 50% [Official Video]",
+    artist: "Official髭男dism",
+    thumbnail: "https://i.ytimg.com/vi/Vv2n31gL85I/hqdefault.jpg",
+    videoId: "Vv2n31gL85I"
+  },
+  {
+    id: "_ciQX22n9NE",
+    title: "Official髭男dism - ホワイトノイズ [Official Video]",
+    artist: "Official髭男dism",
+    thumbnail: "https://i.ytimg.com/vi/_ciQX22n9NE/hqdefault.jpg",
+    videoId: "_ciQX22n9NE"
+  },
+  {
+    id: "p7X-hT1v4Vk",
+    title: "SEKAI NO OWARI「umbrella」【Official Audio】",
+    artist: "SEKAI NO OWARI",
+    thumbnail: "https://i.ytimg.com/vi/p7X-hT1v4Vk/hqdefault.jpg",
+    videoId: "p7X-hT1v4Vk"
+  },
+  {
+    id: "sem3UU-EQJs",
+    title: "Official髭男dism - FIRE GROUND［Official Video］",
+    artist: "Official髭男dism",
+    thumbnail: "https://i.ytimg.com/vi/sem3UU-EQJs/hqdefault.jpg",
+    videoId: "sem3UU-EQJs"
+  },
+  {
+    id: "IzyrINr2Xj4",
+    title: "Official髭男dism - 犬かキャットかで死ぬまで喧嘩しよう！［Official Video］",
+    artist: "Official髭男dism",
+    thumbnail: "https://i.ytimg.com/vi/IzyrINr2Xj4/hqdefault.jpg",
+    videoId: "IzyrINr2Xj4"
+  },
+  {
+    id: "Vho5jBUfR28",
+    title: "Official髭男dism - コーヒーとシロップ［Official Video］",
+    artist: "Official髭男dism",
+    thumbnail: "https://i.ytimg.com/vi/Vho5jBUfR28/hqdefault.jpg",
+    videoId: "Vho5jBUfR28"
+  },
+  {
+    id: "dQb0T5TaRoQ",
+    title: "ESCAPADE",
+    artist: "OFFICIAL HIGE DANDISM - Topic",
+    thumbnail: "https://i.ytimg.com/vi/dQb0T5TaRoQ/hqdefault.jpg",
+    videoId: "dQb0T5TaRoQ"
+  },
+  {
+    id: "hJqYc62NCKo",
+    title: "TheFatRat & Laura Brehm - We'll Meet Again",
+    artist: "TheFatRat",
+    thumbnail: "https://i.ytimg.com/vi/hJqYc62NCKo/hqdefault.jpg",
+    videoId: "hJqYc62NCKo"
+  },
+  {
+    id: "Qi_n6KHDklU",
+    title: "夕暮れ沿い",
+    artist: "OFFICIAL HIGE DANDISM - Topic",
+    thumbnail: "https://i.ytimg.com/vi/Qi_n6KHDklU/hqdefault.jpg",
+    videoId: "Qi_n6KHDklU"
+  },
+  {
+    id: "dh00du4hOGM",
+    title: "清夏",
+    artist: "SHIN - Topic",
+    thumbnail: "https://i.ytimg.com/vi/dh00du4hOGM/hqdefault.jpg",
+    videoId: "dh00du4hOGM"
+  },
+  {
+    id: "dzniwGFO79g",
+    title: "ハルカスミ",
+    artist: "SHIN - Topic",
+    thumbnail: "https://i.ytimg.com/vi/dzniwGFO79g/hqdefault.jpg",
+    videoId: "dzniwGFO79g"
+  },
+  {
+    id: "eJpkUecGyMI",
+    title: "恋花",
+    artist: "SHIN - Topic",
+    thumbnail: "https://i.ytimg.com/vi/eJpkUecGyMI/hqdefault.jpg",
+    videoId: "eJpkUecGyMI"
+  },
+  {
+    id: "l2nqfPAMrSo",
+    title: "Chessboard",
+    artist: "OFFICIAL HIGE DANDISM - Topic",
+    thumbnail: "https://i.ytimg.com/vi/l2nqfPAMrSo/hqdefault.jpg",
+    videoId: "l2nqfPAMrSo"
+  },
+  {
+    id: "xokL7HgM3cQ",
+    title: "ふがらっさ",
+    artist: "吉俣　良 - Topic",
+    thumbnail: "https://i.ytimg.com/vi/xokL7HgM3cQ/hqdefault.jpg",
+    videoId: "xokL7HgM3cQ"
+  },
+  {
+    id: "3LYdaQ1N8RY",
+    title: "最後の恋煩い［Studio Live Session］",
+    artist: "Official髭男dism",
+    thumbnail: "https://i.ytimg.com/vi/3LYdaQ1N8RY/hqdefault.jpg",
+    videoId: "3LYdaQ1N8RY"
+  },
+  {
+    id: "jTTuA0msOyg",
+    title: "Present",
+    artist: "SEKAI NO OWARI - Topic",
+    thumbnail: "https://i.ytimg.com/vi/jTTuA0msOyg/hqdefault.jpg",
+    videoId: "jTTuA0msOyg"
+  },
+  {
+    id: "AYE0l71Trdk",
+    title: "Saidaikouyakusu",
+    artist: "SEKAI NO OWARI - Topic",
+    thumbnail: "https://i.ytimg.com/vi/AYE0l71Trdk/hqdefault.jpg",
+    videoId: "AYE0l71Trdk"
+  },
+  {
+    id: "t-E9h7fJPX4",
+    title: "Official髭男dism - Sanitizer [Official Video]",
+    artist: "Official髭男dism",
+    thumbnail: "https://i.ytimg.com/vi/t-E9h7fJPX4/hqdefault.jpg",
+    videoId: "t-E9h7fJPX4"
   }
 ];
 
